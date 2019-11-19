9dd04259935ccf83f05aa2a04bd8495b26a1811a
Jason Cabot <Jason.Cabot@microsoft.com>
2019-11-19 19:04:26 +0000

    Added threshold for load test

--- loadtest/run.sh
+++ loadtest/run.sh
@@ -3,6 +3,6 @@
 BASE_PATH=$(pwd -P)
 DOCKER_VOLUME_PATH=$(echo $BASE_PATH | sed 's/^\/mnt//')
 
-docker run -e UPLOAD_URL="https://dev.52.155.222.99.nip.io/upload" -v $DOCKER_VOLUME_PATH/loadtest:/var/loadtest -i loadimpact/k6 run -u 10 -d 30s /var/loadtest/upload.js -q --out json=/var/loadtest/results.json
+docker run -e UPLOAD_URL="https://dev.52.155.222.99.nip.io/upload" -v $DOCKER_VOLUME_PATH/loadtest:/var/loadtest -i loadimpact/k6 run /var/loadtest/upload.js -q --out json=/var/loadtest/results.json
 
 node $BASE_PATH/loadtest/k6ToAzure.js $BASE_PATH/loadtest/results.json "Load Test for ${1}" "${2}" "${3}"
--- loadtest/upload.js
+++ loadtest/upload.js
@@ -1,9 +1,18 @@
 import { check } from "k6";
 import http from "k6/http";
 
+export let options = {
+    vus: 10,
+    duration: "1m",
+    thresholds: {
+        http_req_duration: ["p(95)<120000"]
+    },
+    noConnectionReuse: true
+};
+
 const cat = open("/var/loadtest/uploads/test.jpg", "b");
 
-export default function() {
+export default function () {
     var data = { image: http.file(cat, "test.jpg") };
     let res = http.post(`${__ENV.UPLOAD_URL}`, data);
     check(res, {
